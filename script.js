f35060b244a2eb38bbdeffd0da74606a5e5bf496
diego <diego.rodriguez1477@gmail.com>
2024-11-11 21:28:19 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// function cambiarImagen() {
+//  if(  document.getElementById('miDiv1')) {
+//     div.style.backgroundImage = "url(imagen/Black_And_Gold_Minimalist_Instagram_Profile_Picture__3_-removebg-preview.png)";
+//  } else {
+//     div.style.backgroundImage = "url(imagen/Black_And_Gold_Minimalist_Instagram_Profile_Picture__1_-removebg-preview.png)";
+    
+//  }
+   
+
+    
+   
+//   }
